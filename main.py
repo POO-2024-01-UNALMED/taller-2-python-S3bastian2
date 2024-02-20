54af2da1799d9aff2da206494fc43c6efd5f30b4
S3bastian2 <159493363+S3bastian2@users.noreply.github.com>
2024-02-19 19:46:37 -0500

    Creación de clases Auto y Motor
    Se han creado las clases Auto y Motor.

--- main.py
+++ main.py
@@ -14,6 +14,49 @@ class Asiento:
                 self.color = nuevoColor
         else:
             print(f"El color {nuevoColor} no esta permitido para este asiento.")
-            
+
+class Auto:
+     cantidadCreados = 0
+
+     def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        Auto.cantidadCreados += 1
+
+     def cantidadAsientos(self):
+        return sum(1 for asiento in self.asientos if isinstance(asiento, Asiento))
+
+     def verificarIntegridad(self):
+        registros = set()
+        registros.add(self.registro)
+        registros.add(self.motor.registro)
+        for asiento in self.asientos:
+            if asiento is not None:
+                registros.add(asiento.registro)
+        if len(registros) == 1:
+            return "Auto original"
+        else:
+            return "Las piezas no son originales"
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+    
+    def cambiarRegistro(self, nuevoRegistro):
+        self.registro = nuevoRegistro
+    
+    def asignarTipo(self, nuevoTipo):
+        if nuevoTipo.lower() == "electrico" or nuevoTipo.lower() == "gasolina":
+            self.tipo = nuevoTipo
+            print("El tipo de motor cambiado exitosamente")
+        else:
+            print("Tipo de motor no valido. El tipo de motor debe ser electrico o de gasolina.")
+    
 
         
